eb3b1f6e2396db684f20a003f309bdbf55a97304
ToyaYamaguchi <s2342128wb@s.chibakoudai.jp>
2025-06-06 07:56:04 +0000

    php更新

--- htdocs/show-all.php
+++ htdocs/show-all.php
@@ -1,22 +1,25 @@
-<?php
-include 'db.php';  // db.phpを読み込むことでデータベース接続を利用
-
-$query = 'SELECT * FROM products';  // 'products' テーブルからデータを取得
-$stmt = $pdo->query($query);
-?>
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>商品データ</title>
-</head>
-<body>
-    <table>
-        <?php foreach ($stmt as $row): ?>
-        <tr>
-            <td><?= htmlspecialchars($row['id']) ?>, <?= htmlspecialchars($row['name']) ?>, <?= intval($row['price']) ?>, <?= htmlspecialchars($row['stock']) ?></td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
-</body>
+  <head>
+    <meta charset="UTF-8" />
+    <link rel='stylesheet' href='style.css' />
+    <title>全データ表示（その1）</title>
+  </head>
+  <body>
+<?php
+require 'db.php';                               # 接続
+$sql = 'SELECT * FROM table1';                  # SQL文
+$prepare = $db->prepare($sql);                  # 準備
+$prepare->execute();                            # 実行
+$result = $prepare->fetchAll(PDO::FETCH_ASSOC); # 結果の取得
+
+foreach ($result as $row) {
+  $id       = h($row['id']);
+  $varcharA = h($row['varcharA']);
+  $intA     = h($row['intA']);
+  $intB     = h($row['intB']);
+  echo "{$id}, {$varcharA}, {$intA}, {$intB}<br/>";
+}
+?>
+  </body>
 </html>
